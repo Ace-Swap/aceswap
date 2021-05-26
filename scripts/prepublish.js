cf135f58a8f6cf3d4768ed4d139eea36b5b7010a
aceswap <bliu@aeryus.com>
2021-05-26 15:25:39 -0700

    updated the script

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -1,8 +1,8 @@
   
 const fs = require("fs");
 
-for (const contract of fs.readdirSync("./build/contracts")) {
-    const path = "./build/contracts/" + contract;
+for (const contract of fs.readdirSync("./build")) {
+    const path = "./build/" + contract;
     const json = JSON.parse(fs.readFileSync(path).toString());
     fs.writeFileSync(path, JSON.stringify({
         abi: json.abi,
